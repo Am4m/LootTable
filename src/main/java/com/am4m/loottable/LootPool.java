6b87fe9b3ca28b23f53bb35b9788e2c8906d530e
Am4m <mhaidarhammam@gmail.com>
2024-10-27 01:18:02 +0700

    Add functions and predicates

--- src/main/java/com/am4m/loottable/LootPool.java
+++ src/main/java/com/am4m/loottable/LootPool.java
@@ -5,12 +5,9 @@ import com.squareup.moshi.Json;
 import java.util.List;
 
 public record LootPool(
-    @Json(name = "rolls")
-    int rolls,
-
-    @Json(name = "entries")
-    List<LootEntry> entries,
-
-    @Json(name = "bonus_rolls")
-    int bonusRolls
+    @Json(name = "rolls") int rolls,
+    @Json(name = "conditions") List<LootPredicate> conditions,
+    @Json(name = "functions") List<LootFunction> functions,
+    @Json(name = "entries") List<LootEntry> entries,
+    @Json(name = "bonus_rolls") int bonusRolls
 ) {}
